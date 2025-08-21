db0e391f612a890be53b0a547f484d9275ee94c2
Anurag Goyal <Anuragg@cloudeqs.com>
2025-08-21 08:53:29 +0000

    made some final changes to a basic structure in dbt

--- models/dbt_patients.sql
+++ models/dbt_patients.sql
@@ -1,4 +1,11 @@
 with patients as (
-    select first_name as Fname , last_name as Lname , patient_id as pid,age as p_age,blood_type as b_type, adress_id as address_id from agxdb.agxschema.patients
+    select 
+    first_name as Fname , 
+    last_name as Lname , 
+    patient_id as pid,
+    age as p_age,
+    blood_type as b_type, 
+    adress_id as address_id 
+    from agxdb.agxschema.patients
 )
 select * from patients
